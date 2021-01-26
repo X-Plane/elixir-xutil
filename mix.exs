16efe23889fc19b00b248042c17e78da0618fe89
Tyler Young <tyler@x-plane.com>
2021-01-25 19:37:20 -0600

    chore (compile): Disable the silly unused function check (obviously not applicable to a utility library)

--- mix.exs
+++ mix.exs
@@ -9,7 +9,6 @@ defmodule XUtil.MixProject do
       deps_path: "deps",
       lockfile: "mix.lock",
       elixir: "~> 1.11",
-      elixirc_options: [warnings_as_errors: halt_on_warnings?(Mix.env())],
       consolidate_protocols: Mix.env() != :test,
       deps: deps()
     ]
@@ -26,9 +25,4 @@ defmodule XUtil.MixProject do
       {:credo, "~> 1.5.1", only: [:dev, :test], runtime: false}
     ]
   end
-
-  # Clever hack to allow unused functions and the like in test, but not dev or prod:
-  # https://blog.rentpathcode.com/elixir-warnings-as-errors-sometimes-f5a8d2c96b15
-  defp halt_on_warnings?(:test), do: false
-  defp halt_on_warnings?(_), do: true
 end
